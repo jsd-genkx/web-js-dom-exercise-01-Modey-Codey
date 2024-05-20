0c80be76bf01ed862982f791a102bf00637c24e6
Modey Codey <phawngam@hotmail.com>
2024-05-20 13:51:22 +0700

    ex1

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeText() {
+  let elementH2 = document.getElementById("myHeading");
+  elementH2.innerHTML = "Hello JavaScript InnerHTML!";
+}
+
+function changeMe() {
+    let elementH2 = document.getElementById("myHeading1");
+    elementH2.innerHTML = "Hello Modey!";
+  }
+
+
+
+
