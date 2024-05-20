b42f4f4e75cbb76d916b55d15047daea8218faf8
Modey Codey <phawngam@hotmail.com>
2024-05-20 20:36:31 +0700

    Ex1 P2

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ function changeText() {
   elementH2.innerHTML = "Hello JavaScript InnerHTML!";
 }
 
-function changeMe() {
+function greeting() {
     let elementH2 = document.getElementById("myHeading1");
-    elementH2.innerHTML = "Hello Modey!";
+    elementH2.innerHTML = "Alright, mate?";
   }
 
+function addBgColorLeft() {
+  document.getElementById("left").classList.toggle("bg-green-400");
+};
 
+function changeBtnColor() {
+  document.getElementById("btn").classList.toggle("bg-pink-400");
+};
 
+function addBgColorRight() {
+  document.getElementById("right").classList.toggle("bg-blue-400");
+};
 
+function changeBtnColor2() {
+  document.getElementById("btn2").classList.toggle("bg-yellow-200");
+};
